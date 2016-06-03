e4bfae1343974993ee030e5ceaf1c955d272c5b5
karland <anders-betrachtet@online.de>
2016-06-03 15:14:04 +0200

    Minor improvments

--- examples/static-site/index.js
+++ examples/static-site/index.js
@@ -1,27 +1,28 @@
 var Metalsmith  = require('metalsmith');
-var collection  = require('metalsmith-collections');
+var collections = require('metalsmith-collections');
 var layouts     = require('metalsmith-layouts');
 var markdown    = require('metalsmith-markdown');
 var permalinks  = require('metalsmith-permalinks');
 
 
-Metalsmith(__dirname)         // __dirname defined by node.js: name of directory
-                              // that has the currently executing script
-  .metadata({                 // add any variables you want, use them in layouts
+Metalsmith(__dirname)         // __dirname defined by node.js:
+                              // name of current working directory
+  .metadata({                 // add any variable you want
+                              // use them in layout-files
     sitename: "My Static Site & Blog",
     siteurl: "http://example.com/",
-    description: "It's about saying »Hello« to the World.",
+    description: "It's about saying »Hello« to the world.",
     generatorname: "Metalsmith",
     generatorurl: "http://metalsmith.io/"
   })
   .source('./src')            // source directory
   .destination('./build')     // destination directory
-  .clean(true)                // clean destination directory before new build
-  .use(collection({           // group all blog posts by internally
-    posts: 'posts/*.md'       // adding key 'collection':'posts' to all matches
-  }))                         // `collection.posts` now accessible in layouts
+  .clean(true)                // clean destination before
+  .use(collections({          // group all blog posts by internally
+    posts: 'posts/*.md'       // adding key 'collections':'posts'
+  }))                         // use `collections.posts` in layouts
   .use(markdown())            // transpile all md into html
-  .use(permalinks({           // change URLs in permalink URLs
+  .use(permalinks({           // change URLs to permalink URLs
     relative: false           // put css only in /css
   }))
   .use(layouts({              // wrap layouts around html
